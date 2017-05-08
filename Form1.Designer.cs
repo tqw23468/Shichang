b1c0a58a40dad00c52e45fa9a95ce8ed85f0a481
tqw <tqw23468@163.com>
2017-05-08 10:53:33 +0800

    市场

---
+++ Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace Shichang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(382, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(971, 578);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Let\'s go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "均值";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(175, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "100";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(175, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "10";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "周期幅度";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(175, 128);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "12";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "周期长度（天）";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(175, 164);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 21);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "偏移值";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(175, 197);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 21);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.Text = "5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "正态分布方差";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(175, 232);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 21);
+            this.textBox6.TabIndex = 13;
+            this.textBox6.Text = "4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "突发需求时长";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(175, 267);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 21);
+            this.textBox7.TabIndex = 15;
+            this.textBox7.Text = "30";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 267);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 12);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "突发需求幅度";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(175, 329);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 21);
+            this.textBox8.TabIndex = 17;
+            this.textBox8.Text = "4";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 329);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(131, 12);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "突发需求发生概率（%）";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(175, 366);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 21);
+            this.textBox9.TabIndex = 19;
+            this.textBox9.Text = "96";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(30, 366);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 12);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "生成天数";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 433);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(245, 157);
+            this.richTextBox1.TabIndex = 20;
+            this.richTextBox1.Text = "";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(306, 136);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 12);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "不为0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(306, 235);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 12);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "不为0";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(95, 29);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(89, 12);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "市场总需求模型";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(175, 297);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 21);
+            this.textBox10.TabIndex = 25;
+            this.textBox10.Text = "5";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(30, 297);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(113, 12);
+            this.label13.TabIndex = 24;
+            this.label13.Text = "突发需求幅度标准差";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1365, 602);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "市场需求变动模型";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shichang
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //chart1.Series.
+            DataTable dt = default(DataTable);
+            dt = CreateDataTable();
+
+            //设置图表的数据源
+            chart1.DataSource = dt;
+
+            //设置图表Y轴对应项
+            chart1.Series[0].YValueMembers = "Volume1";
+            //chart1.Series[1].YValueMembers = "Volume2";
+
+            //设置图表X轴对应项
+            chart1.Series[0].XValueMember = "Date";
+
+            //绑定数据
+            chart1.DataBind();
+            
+        }
+        private DataTable CreateDataTable()
+        {
+            richTextBox1.Text = "";
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Date");
+            dt.Columns.Add("Volume1");
+            DataRow dr;
+
+            int A = Convert.ToInt32(textBox1.Text);//均值
+            int B = Convert.ToInt32(textBox2.Text);//振幅
+            int T = Convert.ToInt32(textBox3.Text);//周期
+            int P = Convert.ToInt32(textBox4.Text);//偏移
+            int Biaocz = Convert.ToInt32(textBox5.Text); //标准差         
+            int TS = Convert.ToInt32(textBox6.Text);//突发事件持续时长
+            int TF = Convert.ToInt32(textBox7.Text);//突发振幅
+            int TG = Convert.ToInt32(textBox8.Text);//突发概率
+            int TFBC = Convert.ToInt32(textBox10.Text);//突发概率的标准差
+
+            int Tian = Convert.ToInt32(textBox9.Text);//生成多少天数
+           
+            GaussianRNG g = new GaussianRNG();
+
+            //突发需求的四个临时变量（如果需求是一天一天求的话要将这四个保存到数据库中，如果需求是一下子求个几百年存到数据库的话，这四个变量无需存）
+            bool flag = false;//今天在突发中吗
+            int tt = 0;//突发第几天
+            double  tx = 0;//第几天的振幅
+            double tfbc = 0;//最大振幅
+
+
+            Random ran = new Random(unchecked((int)DateTime.Now.Ticks));
+            for (int i = 0; i < Tian; i++) {
+                if (flag == false) {
+                    tx = 0;
+
+                    int RandKey = ran.Next(0, 100);
+                    
+                    if (RandKey < TG)
+                    {
+                        
+                        tfbc =Convert.ToInt32(TF + g.Next() * TFBC+0.5);
+                        flag = true;
+                        tt = 0;
+                        richTextBox1.Text += "第" + i + "天，概率值:" + RandKey.ToString() + ",最大振幅为："+ tfbc+",";
+                    }
+                    else tx = 0;
+                }
+                if (flag == true) {
+                    tx = Math.Floor(tfbc * Math.Sin(Math.PI/TS * tt)+0.5);
+                    richTextBox1.Text += "tx:" + tx.ToString() + ";";
+                    tt++;
+                    if (tt == TS) {
+                        richTextBox1.Text += "\n";
+                        flag = false;
+                        //tx = 0;
+                        tt = 0;
+                        tfbc = 0;
+                    }
+                }
+                dr = dt.NewRow();               
+                dr["Date"] = i+1;
+                dr["Volume1"] = A+B*Math.Sin(2*Math.PI/T*(i+P))+ Math.Floor(g.Next() * Biaocz + 0.5)+tx;
+               
+                dt.Rows.Add(dr);
+            }
+                      
+           
+            return dt;
+        }
+        public class GaussianRNG
+        {
+            int iset;
+            double gset;
+            Random r1, r2;
+            public GaussianRNG()
+            {
+                r1 = new Random(unchecked((int)DateTime.Now.Ticks));
+                r2 = new Random(~unchecked((int)DateTime.Now.Ticks));
+                iset = 0;
+            }
+            public double Next()
+            {
+                double fac, rsq, v1, v2;
+                if (iset == 0)
+                {
+                    do
+                    {
+                        v1 = 2.0 * r1.NextDouble() - 1.0;
+                        v2 = 2.0 * r2.NextDouble() - 1.0;
+                        rsq = v1 * v1 + v2 * v2;
+                    } while (rsq >= 1.0 || rsq == 0.0);
+                    fac = Math.Sqrt(-2.0 * Math.Log(rsq) / rsq);
+                    gset = v1 * fac; iset = 1;
+                    return v2 * fac;
+                }
+                else { iset = 0; return gset; }
+            }
+        }
+    }
+}
